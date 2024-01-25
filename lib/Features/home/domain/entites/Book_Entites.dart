b52407bdc23e0e409cad243dc5a627a0ed1188ce
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-01-26 00:48:15 +0200

    correct Bookmodel and finish inheritance of entity

--- lib/Features/home/domain/entites/Book_Entites.dart
+++ lib/Features/home/domain/entites/Book_Entites.dart
@@ -1,9 +1,17 @@
 class BookEntity {
-  final String image;
+  final String bookId;
+  final String? image;
   final String title;
-  final String author;
-  final num price;
-  final num rate;
+  final String? author;
+  final num? price;
+  final num? rate;
 
-  BookEntity(this.image, this.title, this.author, this.price, this.rate);
+  BookEntity({
+    required this.bookId,
+    required this.image,
+    required this.title,
+    required this.author,
+    required this.price,
+    required this.rate,
+  });
 }
