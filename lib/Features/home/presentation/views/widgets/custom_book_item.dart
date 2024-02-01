3e6a58586d7808bcc125e33d07c7091adee54d86
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-02-02 00:48:54 +0200

    add cached network and show featured books

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,24 +1,23 @@
+import 'package:bookly/Features/home/Data/Models/book_model/book_model.dart';
+import 'package:bookly/Features/home/domain/entites/Book_Entites.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({Key? key}) : super(key: key);
-
+  const CustomBookImage({Key? key, required this.books}) : super(key: key);
+  final BookEntity books;
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.red,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                AssetsData.testImage,
-              ),
-            )),
+      aspectRatio:2.6/4 ,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: CachedNetworkImage(
+          imageUrl: books.image!,
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,4 +1,7 @@
+import 'package:bookly/Features/home/presentation/manager/featuredBooksCubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manager/featuredBooksCubit/featured_books_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'custom_book_item.dart';
 
@@ -7,16 +10,29 @@ class FeaturedBooksListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomBookImage(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksStates>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: ListView.builder(
+                itemCount: state.books.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: CustomBookImage(books: state.books[index]),
+                  );
+                }),
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return SizedBox(
+            child: Text(state.errorMessage),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
